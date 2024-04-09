9f6a43c4cdea1a13395e08ee092c3020f2d303ae
Balbib99 <balbib99@gmail.com>
2024-04-09 13:41:54 +0200

    Actualización de cors con origin *

--- backend/server.js
+++ backend/server.js
@@ -52,10 +52,11 @@
 //     }
 // });
 
+// app.js
+
 const express = require('express');
 const cors = require('cors');
 
-// Se requiere si se quieren utilizar variables de entorno
 require('dotenv').config();
 
 const app = express();
@@ -63,29 +64,30 @@ const PORT = process.env.PORT || 5000;
 
 // Configurar CORS
 const corsOptions = {
-    origin: ['https://web-sensorica.com', 'https://localhost:443', 'http://localhost:5000'],
-    methods: ['GET', 'POST'], // Puedes ajustar los métodos permitidos según tus necesidades
-    allowedHeaders: ['Content-Type', 'Authorization'], // Puedes ajustar las cabeceras permitidas según tus necesidades
-    credentials: true, // Habilita el intercambio de cookies a través de dominios
-    exposedHeaders: 'Access-Control-Allow-Private-Network' // Exponer el encabezado personalizado
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    exposedHeaders: 'Access-Control-Allow-Private-Network'
 };
 
 app.use(cors(corsOptions));
 
-// Middleware para manejar errores de CORS
+// Middleware para manejar errores de CORS de manera general
 app.use((err, req, res, next) => {
-    if (err.name === 'CorsError') {
+    if (err) {
         res.status(403).json({ error: 'CORS Error: ' + err.message });
     } else {
-        next(err);
+        next();
     }
 });
 
-//Llamamos al router
-app.use('/', require('./routes/router'))
+// Llamamos al router
+app.use('/', require('./routes/router'));
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
 
 
