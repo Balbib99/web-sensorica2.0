b3c49db81ed5d63a78421a07e031bea605075380
Balbib99 <balbib99@gmail.com>
2024-04-10 09:10:10 +0200

    vuelta a la configuración unicamente por http

--- backend/server.js
+++ backend/server.js
@@ -55,18 +55,22 @@
 // app.js
 
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
+const cors = require('cors');
+// const https = require('https');
+// const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Configurar la ruta del certificado y la clave privada
-const key = fs.readFileSync('key.pem');
-const cert = fs.readFileSync('cert.pem');
+// // Configurar la ruta del certificado y la clave privada
+// const key = fs.readFileSync('key.pem');
+// const cert = fs.readFileSync('cert.pem');
 
-// Configurar el servidor HTTPS
-const server = https.createServer({ key: key, cert: cert }, app);
+// // Configurar el servidor HTTPS
+// const server = https.createServer({ key: key, cert: cert }, app);
+
+// Middleware para permitir CORS
+app.use(cors());
 
 // Llamamos al router
 app.use('/', require('./routes/router'));
