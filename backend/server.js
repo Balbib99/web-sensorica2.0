ea306d23f85d3b52341c349809ab22c5694abed4
Balbib99 <balbib99@gmail.com>
2024-04-11 10:18:56 +0200

    Primer estado Estable de la web!!, cors cambiadas

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@ const PORT = process.env.PORT || 5000;
 // const server = https.createServer({ key: key, cert: cert }, app);
 
 // Middleware para permitir CORS
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
+
 
 // Llamamos al router
 app.use('/', require('./routes/router'));
